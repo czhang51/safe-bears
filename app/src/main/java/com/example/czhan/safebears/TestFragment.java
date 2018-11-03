0aab1f6f9ee28029807e9065cd9269f0c553b357
Christina Zhang <c.zhang51@gmail.com>
2018-11-03 16:47:25 -0700

    hello here is our main activity : )

---
+++ app/src/main/java/com/example/czhan/safebears/TestFragment.java
@@ -0,0 +1,20 @@
+package com.example.czhan.safebears;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class TestFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.map_fragment, container, false);
+    }
+
+
+}
